c9d3854b35f89038d562a46114d079376a2c99a4
chvg1968 <conradovilla.gmail.com>
2023-01-24 20:47:02 -0500

    Test4

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,37 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 
 galleryItems.forEach(({ preview, original, description }) => {
-  const link = document.createElement('a');
+  const link = document.createElement("a");
   link.href = original;
-  link.classList.add('gallery__link');
-  link.addEventListener('click', (event) => {
+  link.classList.add("gallery__link");
+  link.addEventListener("click", (event) => {
     event.preventDefault();
     const modal = basicLightbox.create(`<img src="${original}">`);
     modal.show();
   });
 
-  const img = document.createElement('img');
+  const img = document.createElement("img");
   img.src = preview;
-  img.setAttribute('data-source', original);
-  img.setAttribute('alt', description);
-  img.setAttribute('title', description);
+  img.setAttribute("data-source", original);
+  img.setAttribute("alt", description);
+  img.setAttribute("title", description);
 
-  img.classList.add('gallery__image');
+  img.classList.add("gallery__image");
   link.appendChild(img);
 
-  const item = document.createElement('div');
-  item.classList.add('gallery__item');
+  const item = document.createElement("div");
+  item.classList.add("gallery__item");
   item.appendChild(link);
 
   gallery.appendChild(item);
 });
 
-(function() {
-    var $gallery = new SimpleLightbox('.gallery a', {captionDelay:250});
+(function () {
+  var $gallery = new SimpleLightbox(".gallery a", { captionDelay: 250 });
 })();
-
