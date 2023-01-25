1d425cff3815fad24c7339926b578ba800b6c684
chvg1968 <conradovilla.gmail.com>
2023-01-24 19:52:03 -0500

    Test1

---
+++ js/01-gallery.js
@@ -0,0 +1,29 @@
+import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(({ preview, original, description }) => {
+  const link = document.createElement('a');
+  link.href = original;
+  link.classList.add('gallery__link');
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const modal = basicLightbox.create(`<img src="${original}">`);
+    modal.show();
+  });
+
+  const img = document.createElement('img');
+  img.src = preview;
+  img.setAttribute('data-source', original);
+  img.setAttribute('alt', description);
+  img.classList.add('gallery__image');
+  link.appendChild(img);
+
+  const item = document.createElement('div');
+  item.classList.add('gallery__item');
+  item.appendChild(link);
+
+  gallery.appendChild(item);
+});
---
+++ js/02-lightbox.js
@@ -0,0 +1,37 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach(({ preview, original, description }) => {
+  const link = document.createElement('a');
+  link.href = original;
+  link.classList.add('gallery__link');
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const modal = basicLightbox.create(`<img src="${original}">`);
+    modal.show();
+  });
+
+  const img = document.createElement('img');
+  img.src = preview;
+  img.setAttribute('data-source', original);
+  img.setAttribute('alt', description);
+  img.setAttribute('title', description);
+
+  img.classList.add('gallery__image');
+  link.appendChild(img);
+
+  const item = document.createElement('div');
+  item.classList.add('gallery__item');
+  item.appendChild(link);
+
+  gallery.appendChild(item);
+});
+
+(function() {
+    var $gallery = new SimpleLightbox('.gallery a', {captionDelay:250});
+})();
+
